87aac8dcbc36d85af5b3c8fa33f823821fd67502
CarlosCueto <4bitsdesign@gmail.com>
2014-01-12 06:26:09 +0100

    Decodificador: añadida entrada enable.

--- HexCounter/HexDecoder.vhd
+++ HexCounter/HexDecoder.vhd
@@ -31,7 +31,9 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity HEXDECODER is
     Port ( HEX : in  std_logic_vector (3 downto 0);
-           LED : out  std_logic_vector (7 downto 0));
+           LED : out  std_logic_vector (7 downto 0);
+			  EN : in std_logic
+	 );
 end HEXDECODER;
 
 architecture DATAFLOW of HEXDECODER is
@@ -45,23 +47,24 @@ begin
 --     ---
 --      d
    
-   with HEX SELect
-   LED(7 downto 1)<= "1001111" when "0001",   --1
-							"0010010" when "0010",   --2
-							"0000110" when "0011",   --3
-							"1001100" when "0100",   --4
-							"0100100" when "0101",   --5
-							"0100000" when "0110",   --6
-							"0001111" when "0111",   --7
-							"0000000" when "1000",   --8
-							"0000100" when "1001",   --9
-							"0001000" when "1010",   --A
-							"1100000" when "1011",   --b
-							"0110001" when "1100",   --C
-							"1000010" when "1101",   --d
-							"0110000" when "1110",   --E
-							"0111000" when "1111",   --F
-							"0000001" when others;   --0
+   
+   LED(7 downto 1)<= "1001111" when HEX = "0001" and EN = '1' else   --1
+							"0010010" when HEX = "0010" and EN = '1' else   --2
+							"0000110" when HEX = "0011" and EN = '1' else   --3
+							"1001100" when HEX = "0100" and EN = '1' else   --4
+							"0100100" when HEX = "0101" and EN = '1' else   --5
+							"0100000" when HEX = "0110" and EN = '1' else   --6
+							"0001111" when HEX = "0111" and EN = '1' else   --7
+							"0000000" when HEX = "1000" and EN = '1' else   --8
+							"0000100" when HEX = "1001" and EN = '1' else   --9
+							"0001000" when HEX = "1010" and EN = '1' else   --A
+							"1100000" when HEX = "1011" and EN = '1' else   --b
+							"0110001" when HEX = "1100" and EN = '1' else   --C
+							"1000010" when HEX = "1101" and EN = '1' else   --d
+							"0110000" when HEX = "1110" and EN = '1' else   --E
+							"0111000" when HEX = "1111" and EN = '1' else   --F
+							"1111110" when EN = '0' else
+							"0000001" ;   --0
 	
 	LED(0) <= '0';
 
